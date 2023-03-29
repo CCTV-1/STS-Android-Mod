c413149117514cd04401247a16f230732f9c2278
CCTV-1 <script.tar.gz@gmail.com>
2023-03-29 22:30:01 +0800

    fix:miss check card type.

--- ModScripts/NewPowers/PlunderPower.ts
+++ ModScripts/NewPowers/PlunderPower.ts
@@ -1,12 +1,17 @@
+import { CardType } from "../enums.js";
+import { AbstractCard } from "../NativeClassWrap/AbstractCard.js";
 import { AbstractPower, NewPowerVFuncType } from "../NativeClassWrap/AbstractPower.js"
 import { PatchHelper } from "../PatchHelper.js";
 
 export const PlunderPower = (ownerCreature: NativePointer, amount: number): NativePointer => {
     let vfuncs: NewPowerVFuncType = {
         onUseCard: (thisPtr: NativePointer, cardPtr: NativePointer, useCardActionPtr: NativePointer) => {
-            let wrapPower = new AbstractPower(thisPtr);
-            let currentPlayer = PatchHelper.STSGlobalVars.AbstractDungeon_player;
-            currentPlayer.gainGold(wrapPower.amount*3);
+            let wrapCard = new AbstractCard(cardPtr);
+            if (wrapCard.type === CardType.ATTACK) {                
+                let wrapPower = new AbstractPower(thisPtr);
+                let currentPlayer = PatchHelper.STSGlobalVars.AbstractDungeon_player;
+                currentPlayer.gainGold(wrapPower.amount*3);
+            }
         },
         updateDescription: (thisPtr: NativePointer) => {
             let wrapPower = new AbstractPower(thisPtr);
