8a8b0d4608f8739f4e5bb97c07d08080af3554cb
CCTV-1 <script.tar.gz@gmail.com>
2023-03-30 20:28:03 +0800

    wrap img/imgRegion member.

--- ModScripts/NativeClassWrap/PowerTip.ts
+++ ModScripts/NativeClassWrap/PowerTip.ts
@@ -7,6 +7,26 @@ export class PowerTip extends NativeClassWrapper {
         super(CthisPtr);
     }
 
+    /**
+     * GDX::graphics::Texture *
+     */
+    get img() {
+        return this.readOffsetPointer(0x8);
+    }
+    set img(value) {
+        this.writeOffsetPointer(0x8, value);
+    }
+
+    /**
+     * GDX::graphics::g2d::TextureAtlas.AtlasRegion *
+     */
+    get imgRegion() {
+        return this.readOffsetPointer(0xC);
+    }
+    set imgRegion(value) {
+        this.writeOffsetPointer(0xC, value);
+    }
+
     get header() {
         return this.readOffsetJString(0x10).content;
     }
