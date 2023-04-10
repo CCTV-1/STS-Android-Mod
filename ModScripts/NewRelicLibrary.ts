278e40a07b2330095decb6a5ef4b6cd0f55a5b74
CCTV-1 <script.tar.gz@gmail.com>
2023-04-10 20:43:36 +0800

    new relic:'TheRingOfPower'.

--- ModScripts/NewRelicLibrary.ts
+++ ModScripts/NewRelicLibrary.ts
@@ -1,8 +1,10 @@
 import { STSRelicCtor } from "./NativeClassWrap/AbstractRelic.js";
 import { Mechanization } from "./NewRelics/Mechanization.js";
 import { TheOneRing } from "./NewRelics/TheOneRing.js";
+import { TheRingOfPower } from "./NewRelics/TheRingOfPower.js";
 
 export const newRelicLibrary: STSRelicCtor[] = [
     Mechanization,
     TheOneRing,
+    TheRingOfPower,
 ];
---
+++ ModScripts/NewRelics/TheRingOfPower.ts
@@ -0,0 +1,29 @@
+import { AbstractDungeon } from "../NativeClassWrap/AbstractDungeon.js";
+import { AbstractRelic, NewRelicVFuncType } from "../NativeClassWrap/AbstractRelic.js";
+import { NativeActions } from "../NativeFuncWrap/NativeActions.js";
+import { NativePowers } from "../NativeFuncWrap/NativePowers.js";
+import { LandingSound, RelicTier } from "../enums.js";
+
+export const TheRingOfPower = (thisPtr: NativePointer): NativePointer => {
+    const vfuncs: NewRelicVFuncType = {
+        atTurnStart: (thisPtr: NativePointer) => {
+            const currentPlayer = AbstractDungeon.getInstance().player.rawPtr;
+            const wrapRelic = new AbstractRelic(thisPtr);
+            const strengthPower = NativePowers.Common.Strength.Ctor(currentPlayer, 4);
+            let applyPowerAction = NativeActions.common.ApplyPower.Ctor2(currentPlayer, currentPlayer, strengthPower, 1);
+            wrapRelic.addToBot(applyPowerAction);
+            const dexterityPower = NativePowers.Common.Dexterity.Ctor(currentPlayer, -2);
+            applyPowerAction = NativeActions.common.ApplyPower.Ctor2(currentPlayer, currentPlayer, dexterityPower, -1);
+            wrapRelic.addToBot(applyPowerAction);
+            wrapRelic.flash();
+        },
+        makeCopy: (thisPtr: NativePointer) => {
+            let copyObj = TheRingOfPower(thisPtr);
+            return copyObj;
+        },
+    };
+
+    let relicObj = AbstractRelic.NewRelicCtor("TheRingOfPower", "力量魔戒", "在每回合开始时获得4点力量并失去2点敏捷。", "手握日月摘星辰，世间无我这般人。", "TheRingOfPower.png", RelicTier.BOSS, LandingSound.HEAVY, vfuncs);
+
+    return relicObj;
+};
