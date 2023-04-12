8c3e7bf7ef547f58f3d48cc729030bb03db6b1b5
CCTV-1 <script.tar.gz@gmail.com>
2023-04-12 12:47:41 +0800

    remake ability.

--- ModScripts/NewCards/DemonicPurge.ts
+++ ModScripts/NewCards/DemonicPurge.ts
@@ -1,7 +1,8 @@
 import { AbstractCard, NewCardVFuncType, STSCardCtor } from "../NativeClassWrap/AbstractCard.js";
 import { AbstractDungeon } from "../NativeClassWrap/AbstractDungeon.js";
 import { NativeActions } from "../NativeFuncWrap/NativeActions.js";
-import { CardType, CardColor, CardRarity, CardTarget, DamageType } from "../enums.js";
+import { NativePowers } from "../NativeFuncWrap/NativePowers.js";
+import { CardType, CardColor, CardRarity, CardTarget, DamageType, AttackEffect } from "../enums.js";
 
 const vfuncs: NewCardVFuncType = {
     use: (thisPtr: NativePointer, playerPtr: NativePointer, monsterPtr: NativePointer) => {
@@ -9,44 +10,38 @@ const vfuncs: NewCardVFuncType = {
         let currentPlayer = AbstractDungeon.getInstance().player;
         const handList = currentPlayer.hand.group;
         const handSize = handList.size;
-        const drawPileList = currentPlayer.drawPile.group;
-        let drawPileSize = drawPileList.size;
-        const numCards = handSize + drawPileSize;
 
-        let exhaustAction = NativeActions.common.Exhaust.Ctor(99, true, false, false);
+        const exhaustAction = NativeActions.common.Exhaust.Ctor(99, true, false, false);
         wrapCard.addToBot(exhaustAction);
 
-        while (drawPileSize > 0) {
-            let drawAction = NativeActions.common.DrawCard.Ctor(playerPtr, drawPileSize % 10, false);
-            wrapCard.addToBot(drawAction);
-            drawPileSize = drawPileSize - drawPileSize % 10;
-            let exhaustAction = NativeActions.common.Exhaust.Ctor(99, true, false, false);
-            wrapCard.addToBot(exhaustAction);
-        }
+        const loseHandSizeHPAction = NativeActions.common.LoseHP.Ctor(playerPtr, playerPtr, handSize, AttackEffect.NONE);
+        wrapCard.addToBot(loseHandSizeHPAction);
 
-        let removeDebuffAction = NativeActions.unique.RemoveDebuffs.Ctor(playerPtr);
+        const strPower = NativePowers.Common.Strength.Ctor(playerPtr, handSize);
+        const applyPowerAction = NativeActions.common.ApplyPower.Ctor2(playerPtr, playerPtr, strPower, handSize);
+        wrapCard.addToBot(applyPowerAction);
+
+        const removeDebuffAction = NativeActions.unique.RemoveDebuffs.Ctor(playerPtr);
         wrapCard.addToBot(removeDebuffAction);
-
-        let makeTempCardAtBottomOfDeckAction = NativeActions.common.MakeTempCardAtBottomOfDeck.Ctor(numCards);
-        wrapCard.addToBot(makeTempCardAtBottomOfDeckAction);
     },
     upgrade: (thisPtr: NativePointer) => {
-        let wrapCard = new AbstractCard(thisPtr);
+        const wrapCard = new AbstractCard(thisPtr);
         if (!wrapCard.upgraded) {
-            wrapCard.upgradeBaseCost(2);
+            wrapCard.upgradeBaseCost(0);
             wrapCard.upgradeName();
         }
     },
     makeCopy: (thisPtr: NativePointer) => {
-        let copyObj = DemonicPurge(thisPtr);
+        const copyObj = DemonicPurge(thisPtr);
         return copyObj;
     },
 };
 
 export const DemonicPurge: STSCardCtor = (thisPtr: NativePointer) => {
-    let wrapCard = AbstractCard.NewCardCtor("DemonicPurge", "邪恶净化", "red/skill/DemonicPurge", 3,
-        "消耗手牌和抽牌堆，在牌库底生成等量的牌。 NL 清除所有负面状态。",
+    let wrapCard = AbstractCard.NewCardCtor("DemonicPurge", "邪恶净化", "red/skill/DemonicPurge", 1,
+        "消耗所有手牌。失去等量的生命。获得等量的力量。 NL 清除所有负面状态。 NL 消耗。",
         CardType.SKILL, CardColor.RED, CardRarity.RARE, CardTarget.NONE, DamageType.NORMAL, vfuncs);
 
+    wrapCard.exhaust = true;
     return wrapCard.rawPtr;
 };
