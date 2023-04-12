538903a043358fb7f90eccf292fa49806b8f092a
CCTV-1 <script.tar.gz@gmail.com>
2023-04-12 19:04:11 +0800

    new relic:'TheSoulRing'.

--- ModScripts/NewRelicLibrary.ts
+++ ModScripts/NewRelicLibrary.ts
@@ -1,10 +1,12 @@
 import { STSRelicCtor } from "./NativeClassWrap/AbstractRelic.js";
 import { Mechanization } from "./NewRelics/Mechanization.js";
 import { TheOneRing } from "./NewRelics/TheOneRing.js";
-import { TheRingOfPower } from "./NewRelics/TheRingOfPower.js";
+import { TheSoulRing } from "./NewRelics/TheSoulRing.js";
+import { TheStrengthRing } from "./NewRelics/TheStrengthRing.js";
 
 export const newRelicLibrary: STSRelicCtor[] = [
     Mechanization,
     TheOneRing,
-    TheRingOfPower,
+    TheStrengthRing,
+    TheSoulRing,
 ];
