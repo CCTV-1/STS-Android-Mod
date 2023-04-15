e3c57ccf07da461b17631443bf969567036b6bd1
CCTV-1 <script.tar.gz@gmail.com>
2023-04-15 09:54:25 +0800

    if randomCard can't upgrade,change to draw random number cards,then discard random number cards.

--- ModScripts/NewActions/ChaosRingAction.ts
+++ ModScripts/NewActions/ChaosRingAction.ts
@@ -2,6 +2,7 @@ import { ModUtility } from "../ModUtility.js";
 import { AbstractCard } from "../NativeClassWrap/AbstractCard.js";
 import { AbstractDungeon } from "../NativeClassWrap/AbstractDungeon.js";
 import { AbstractGameAction, NewGameActionVFuncType } from "../NativeClassWrap/AbstractGameAction.js"
+import { NativeActions } from "../NativeFuncWrap/NativeActions.js";
 
 const vfuncs: NewGameActionVFuncType = {
     update: (thisPtr: NativePointer) => {
@@ -14,12 +15,20 @@ const vfuncs: NewGameActionVFuncType = {
         }
         const randCard = playerHand.getRandomCard2(true);
         const wrapCard = new AbstractCard(randCard);
-        wrapCard.upgrade();
-        wrapCard.upgradeBaseCost(ModUtility.FakeRandom(0, 3));
-        wrapCard.upgradeDamage(ModUtility.FakeRandom(-wrapCard.baseDamage, wrapCard.baseDamage * 4));
-        wrapCard.upgradeBlock(ModUtility.FakeRandom(-wrapCard.baseBlock, wrapCard.baseBlock * 4));
-        wrapCard.upgradeMagicNumber(ModUtility.FakeRandom(-wrapCard.baseMagicNumber, wrapCard.baseMagicNumber * 4));
-        wrapCard.flash();
+        if (wrapCard.canUpgrade()) {
+            wrapCard.upgrade();
+            wrapCard.upgradeBaseCost(ModUtility.FakeRandom(0, 3));
+            wrapCard.upgradeDamage(ModUtility.FakeRandom(-wrapCard.baseDamage, wrapCard.baseDamage * 4));
+            wrapCard.upgradeBlock(ModUtility.FakeRandom(-wrapCard.baseBlock, wrapCard.baseBlock * 4));
+            wrapCard.upgradeMagicNumber(ModUtility.FakeRandom(-wrapCard.baseMagicNumber, wrapCard.baseMagicNumber * 4));
+            wrapCard.flash();
+        } else {
+            const DrawCount = ModUtility.FakeRandom(0,3);
+            const drawAction = NativeActions.common.DrawCard.Ctor(currentPlayer.rawPtr, DrawCount, false);
+            const randDiscardAction = NativeActions.common.Discard.Ctor(currentPlayer.rawPtr, currentPlayer.rawPtr, ModUtility.FakeRandom(0, 2*DrawCount));
+            wrapAction.addToBot(drawAction);
+            wrapAction.addToBot(randDiscardAction);
+        }
         wrapAction.isDone = true;
     }
 };
