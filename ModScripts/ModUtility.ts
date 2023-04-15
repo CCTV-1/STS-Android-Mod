c80bfc5110af0c54b5766e3dac4ef85063e349ab
CCTV-1 <script.tar.gz@gmail.com>
2023-04-15 11:50:08 +0800

    don't work,roll back implement.

--- ModScripts/ModUtility.ts
+++ ModScripts/ModUtility.ts
@@ -4,24 +4,13 @@ import { AbstractPlayer } from "./NativeClassWrap/AbstractPlayer.js";
 import { AbstractRoom } from "./NativeClassWrap/AbstractRoom.js";
 import { MapRoomNode } from "./NativeClassWrap/MapRoomNode.js";
 import { MonsterGroup } from "./NativeClassWrap/MonsterGroup.js";
-import { Random } from "./NativeClassWrap/Random.js";
 import { NativeSTDLib } from "./NativeFuncWrap/NativeSTDLib.js";
 import { NativeVFX } from "./NativeFuncWrap/NativeVFX.js";
 import { PatchHelper } from "./PatchHelper.js";
 
-
 export class ModUtility {
-    static RNGRef: Random | undefined = undefined
-    
     static FakeRandom(min: number, max: number): number {
-        if (ModUtility.RNGRef === undefined) {
-            const eventRng = AbstractDungeon.getInstance().eventRng;
-            if (eventRng.isNull()) {
-                return Math.floor(Math.random() * (max - min + 1)) + min;
-            }
-            ModUtility.RNGRef = new Random(eventRng);
-        }
-        return ModUtility.RNGRef.randomI32_2(min, max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     static UpgradeRandomCard(currentPlayer: AbstractPlayer) {
