f4169c335cd7970c4a6d734237b6e567de327a1b
CCTV-1 <script.tar.gz@gmail.com>
2023-04-16 10:56:47 +0800

    handle bound condition.

--- ModScripts/NewActions/ChaosRingAction.ts
+++ ModScripts/NewActions/ChaosRingAction.ts
@@ -18,15 +18,23 @@ const vfuncs: NewGameActionVFuncType = {
         const eventRng = new Random(AbstractDungeon.getInstance().eventRng);
         if (wrapCard.canUpgrade()) {
             wrapCard.upgrade();
-            wrapCard.upgradeBaseCost(eventRng.randomI32_2(0, 3));
-            wrapCard.upgradeDamage(eventRng.randomI32_2(-wrapCard.baseDamage, wrapCard.baseDamage * 4));
-            wrapCard.upgradeBlock(eventRng.randomI32_2(-wrapCard.baseBlock, wrapCard.baseBlock * 4));
-            wrapCard.upgradeMagicNumber(eventRng.randomI32_2(-wrapCard.baseMagicNumber, wrapCard.baseMagicNumber * 4));
+            if (wrapCard.cost >= 0) {
+                wrapCard.upgradeBaseCost(eventRng.randomI32_2(0, 3));
+            }
+            if (wrapCard.baseDamage >= 0) {
+                wrapCard.upgradeDamage(eventRng.randomI32_2(-wrapCard.baseDamage, wrapCard.baseDamage * 4));
+            }
+            if (wrapCard.baseBlock >= 0) {
+                wrapCard.upgradeBlock(eventRng.randomI32_2(-wrapCard.baseBlock, wrapCard.baseBlock * 4));
+            }
+            if (wrapCard.baseMagicNumber >= 0) {
+                wrapCard.upgradeMagicNumber(eventRng.randomI32_2(-wrapCard.baseMagicNumber, wrapCard.baseMagicNumber * 4));
+            }
             wrapCard.flash();
         } else {
-            const DrawCount = eventRng.randomI32_2(0,3);
+            const DrawCount = eventRng.randomI32_2(1, 3);
             const drawAction = NativeActions.common.DrawCard.Ctor(currentPlayer.rawPtr, DrawCount, false);
-            const randDiscardAction = NativeActions.common.Discard.Ctor(currentPlayer.rawPtr, currentPlayer.rawPtr, eventRng.randomI32_2(0, 2*DrawCount));
+            const randDiscardAction = NativeActions.common.Discard.Ctor(currentPlayer.rawPtr, currentPlayer.rawPtr, eventRng.randomI32_2(1, 2 * DrawCount));
             wrapAction.addToBot(drawAction);
             wrapAction.addToBot(randDiscardAction);
         }
