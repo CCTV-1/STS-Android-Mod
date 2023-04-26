aea1b17b907e021f2f09984b9332627d53793155
CCTV-1 <script.tar.gz@gmail.com>
2023-04-26 19:25:27 +0800

    version 2.

--- ModScripts/NewCards/TreasureCruise.ts
+++ ModScripts/NewCards/TreasureCruise.ts
@@ -1,4 +1,5 @@
 import { AbstractCard, NewCardVFuncType, STSCardCtor } from "../NativeClassWrap/AbstractCard.js";
+import { GameActionManager } from "../NativeClassWrap/GameActionManager.js";
 import { NativeActions } from "../NativeFuncWrap/NativeActions.js";
 import { CardColor, CardRarity, CardTarget, CardType, DamageType } from "../enums.js";
 
@@ -17,6 +18,20 @@ const vfuncs: NewCardVFuncType = {
             wrapCard.isCostModifiedForTurn = true;
         }
     },
+    triggerWhenDrawn: (thisPtr) => {
+        const wrapCard = new AbstractCard(thisPtr);
+        const actionManagerConstant = GameActionManager.getConstantHandle();
+        const currentCost = wrapCard.cost - actionManagerConstant.totalDiscardedThisTurn;
+        if (currentCost >= 0) {
+            wrapCard.costForTurn = currentCost;
+        } else {
+            wrapCard.costForTurn = 0;
+        }
+
+        if (wrapCard.costForTurn != wrapCard.cost) {
+            wrapCard.isCostModifiedForTurn = true;
+        }
+    },
     upgrade: (thisPtr: NativePointer) => {
         const wrapCard = new AbstractCard(thisPtr);
         if (!wrapCard.upgraded) {
@@ -31,8 +46,7 @@ const vfuncs: NewCardVFuncType = {
 };
 
 export const TreasureCruise: STSCardCtor = (thisPtr: NativePointer) => {
-    let wrapCard = AbstractCard.NewCardCtor("TreasureCruise", "宝船巡游", "green/skill/TreasureCruise", 4, "本回合中每弃一张牌费用便减少[E]。 NL 抓 !M! 张。", CardType.SKILL,
-        CardColor.GREEN, CardRarity.COMMON, CardTarget.NONE, DamageType.NORMAL, vfuncs);
+    let wrapCard = AbstractCard.NewCardCtor("TreasureCruise", "宝船巡游", "green/skill/TreasureCruise", 4, "每弃一张牌耗能便减少 [G] 。 NL 抓 !M! 张。", CardType.SKILL, CardColor.GREEN, CardRarity.COMMON, CardTarget.NONE, DamageType.NORMAL, vfuncs);
 
     wrapCard.baseMagicNumber = 3;
     wrapCard.magicNumber = 3;
