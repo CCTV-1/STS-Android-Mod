b06fc2805473b7debddd8aba6b7e57b5bf217c52
CCTV-1 <script.tar.gz@gmail.com>
2023-04-30 19:26:36 +0800

    flash on atBattleStart.

--- ModScripts/NewRelics/TheOneRing.ts
+++ ModScripts/NewRelics/TheOneRing.ts
@@ -4,31 +4,30 @@ import { NativeSTDLib } from "../NativeFuncWrap/NativeSTDLib.js";
 import { AbstractDungeon } from "../NativeClassWrap/AbstractDungeon.js";
 
 const vfuncs: NewRelicVFuncType = {
-    getUpdatedDescription: (thisPtr: NativePointer) => {
-        return NativeSTDLib.JString.Ctor("在每场战斗开始时失去三分之一的生命，在每回合开始时获得 [E]  [E]  [E] 。");
-    },
     onEquip: (thisPtr: NativePointer) => {
-        let currentPlayer = AbstractDungeon.getInstance().player;
-        let energy = currentPlayer.energy;
-        energy.energyMaster += 3;
+        const currentPlayer = AbstractDungeon.getInstance().player;
+        const energyManager = currentPlayer.energy;
+        energyManager.energyMaster += 3;
     },
     onUnequip: (thisPtr: NativePointer) => {
-        let currentPlayer = AbstractDungeon.getInstance().player;
-        let energy = currentPlayer.energy;
-        energy.energyMaster -= 3;
+        const currentPlayer = AbstractDungeon.getInstance().player;
+        const energyManager = currentPlayer.energy;
+        energyManager.energyMaster -= 3;
     },
     atBattleStart: (thisPtr: NativePointer) => {
-        let currentPlayer = AbstractDungeon.getInstance().player;
+        const currentPlayer = AbstractDungeon.getInstance().player;
+        const wrapRelic = new AbstractRelic(thisPtr);
         currentPlayer.heal(-currentPlayer.currentHealth / 3, true);
+        wrapRelic.flash();
     },
     makeCopy: (thisPtr: NativePointer) => {
-        let copyObj = TheOneRing(thisPtr);
+        const copyObj = TheOneRing(thisPtr);
         return copyObj;
     },
 };
 
 export const TheOneRing = (thisPtr: NativePointer): NativePointer => {
-    let relicObj = AbstractRelic.NewRelicCtor("TheOneRing", "至尊魔戒", "在每场战斗开始时失去三分之一的生命，在每回合开始时获得 [E]  [E]  [E] 。", "无穷无尽的力量令人沉醉。", "TheOneRing.png", RelicTier.BOSS, LandingSound.HEAVY, vfuncs);
+    const relicObj = AbstractRelic.NewRelicCtor("TheOneRing", "至尊魔戒", "在每场战斗开始时失去三分之一的生命，在每回合开始时获得 [E]  [E]  [E] 。", "无穷无尽的力量令人沉醉。", "TheOneRing.png", RelicTier.BOSS, LandingSound.HEAVY, vfuncs);
 
     return relicObj;
 };
