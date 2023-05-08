7e698faa89293b2b25b37fba7fb7836abdef56f2
CCTV-1 <script.tar.gz@gmail.com>
2023-05-08 12:51:46 +0800

    refactor ability.

--- ModScripts/NewCards/BasicAttackRed.ts
+++ ModScripts/NewCards/BasicAttackRed.ts
@@ -2,34 +2,38 @@ import { AbstractCard, NewCardVFuncType } from "../NativeClassWrap/AbstractCard.
 import { AttackEffect, CardColor, CardRarity, CardTarget, CardType, DamageType } from "../enums.js";
 import { NativeActions } from "../NativeFuncWrap/NativeActions.js";
 import { NativeCards } from "../NativeFuncWrap/NativeCards.js";
+import { NativePowers } from "../NativeFuncWrap/NativePowers.js";
 
 const vfuncs: NewCardVFuncType = {
     use: (thisPtr: NativePointer, playerPtr: NativePointer, monsterPtr: NativePointer) => {
-        let wrapCard = new AbstractCard(thisPtr);
-        let dmgInfoObj = NativeCards.DamageInfo.Ctor(playerPtr, wrapCard.damage, wrapCard.damageTypeForTurn);
-        let damageAction = NativeActions.common.Damage.Ctor(monsterPtr, dmgInfoObj, AttackEffect.SLASH_DIAGONAL);
-        let targetCard = NativeCards.status.Burn.Ctor();
-        let makeTempCardInHandAction = NativeActions.common.MakeTempCardInHand.Ctor(targetCard, 1, true);
+        const wrapCard = new AbstractCard(thisPtr);
+        const dmgInfoObj = NativeCards.DamageInfo.Ctor(playerPtr, wrapCard.damage, wrapCard.damageTypeForTurn);
+        const damageAction = NativeActions.common.Damage.Ctor(monsterPtr, dmgInfoObj, AttackEffect.SLASH_DIAGONAL);
+        const vulnerablePower = NativePowers.Common.Vulnerable.Ctor(monsterPtr, wrapCard.magicNumber, false);
+        const applyPowerAction = NativeActions.common.ApplyPower.Ctor2(monsterPtr, playerPtr, vulnerablePower, wrapCard.magicNumber);
         wrapCard.addToBot(damageAction);
-        wrapCard.addToBot(makeTempCardInHandAction);
+        wrapCard.addToBot(applyPowerAction);
     },
     upgrade: (thisPtr: NativePointer) => {
-        let wrapCard = new AbstractCard(thisPtr);
+        const wrapCard = new AbstractCard(thisPtr);
         if (!wrapCard.upgraded) {
             wrapCard.upgradeName();
             wrapCard.upgradeDamage(3);
+            wrapCard.upgradeMagicNumber(3);
         }
     },
     makeCopy: (thisPtr: NativePointer) => {
-        let copyObj = BasicAttackRed(thisPtr);
+        const copyObj = BasicAttackRed(thisPtr);
         return copyObj;
     },
 };
 
 export const BasicAttackRed = (thisPtr: NativePointer): NativePointer => {
-    let wrapCard = AbstractCard.NewCardCtor("BasicAttack_R", "熔岩击", "red/attack/strike", 1, "造成 !D! 点伤害，将一张灼伤放入你的手牌。", CardType.ATTACK,
+    const wrapCard = AbstractCard.NewCardCtor("BasicAttack_R", "熔岩击", "red/attack/strike", 1, "造成 !D! 点伤害，给与 !M! 层易伤。", CardType.ATTACK,
         CardColor.RED, CardRarity.BASIC, CardTarget.ENEMY, DamageType.NORMAL, vfuncs);
-    wrapCard.baseDamage = 9;
+    wrapCard.baseDamage = 6;
+    wrapCard.baseMagicNumber = 2;
+    wrapCard.magicNumber = 2;
 
     return wrapCard.rawPtr;
 };
--- ModScripts/NewCards/BasicDefendRed.ts
+++ ModScripts/NewCards/BasicDefendRed.ts
@@ -1,32 +1,41 @@
 import { AbstractCard, NewCardVFuncType } from "../NativeClassWrap/AbstractCard.js";
-import { AttackEffect, CardColor, CardRarity, CardTarget, CardType, DamageType } from "../enums.js";
+import { CardColor, CardRarity, CardTarget, CardType, DamageType } from "../enums.js";
 import { NativeActions } from "../NativeFuncWrap/NativeActions.js";
+import { NativePowers } from "../NativeFuncWrap/NativePowers.js";
+import { AbstractDungeon } from "../NativeClassWrap/AbstractDungeon.js";
 
 const vfuncs: NewCardVFuncType = {
     use: (thisPtr: NativePointer, playerPtr: NativePointer, monsterPtr: NativePointer) => {
-        let wrapCard = new AbstractCard(thisPtr);
-        let damageAction = NativeActions.common.GainBlock.Ctor2(playerPtr, playerPtr, wrapCard.block);
-        wrapCard.addToBot(damageAction);
-        let loseHpAction = NativeActions.common.LoseHP.Ctor(playerPtr, playerPtr, 2, AttackEffect.NONE);
-        wrapCard.addToBot(loseHpAction);
+        const wrapCard = new AbstractCard(thisPtr);
+        const gainBlockAction = NativeActions.common.GainBlock.Ctor2(playerPtr, playerPtr, wrapCard.block);
+        wrapCard.addToBot(gainBlockAction);
+    },
+    triggerOnExhaust: (thisPtr) => {
+        const wrapCard = new AbstractCard(thisPtr);
+        const currentPlayer = AbstractDungeon.getInstance().player;
+        const metallicizePower = NativePowers.Common.Metallicize.Ctor(currentPlayer.rawPtr, wrapCard.magicNumber);
+        const applyPowerAction = NativeActions.common.ApplyPower.Ctor2(currentPlayer.rawPtr, currentPlayer.rawPtr, metallicizePower, wrapCard.magicNumber);
+        wrapCard.addToBot(applyPowerAction);
     },
     upgrade: (thisPtr: NativePointer) => {
-        let wrapCard = new AbstractCard(thisPtr);
+        const wrapCard = new AbstractCard(thisPtr);
         if (!wrapCard.upgraded) {
             wrapCard.upgradeName();
-            wrapCard.upgradeBlock(3);
+            wrapCard.upgradeBlock(2);
+            wrapCard.upgradeMagicNumber(2);
         }
     },
     makeCopy: (thisPtr: NativePointer) => {
-        let copyObj = BasicDefendRed(thisPtr);
+        const copyObj = BasicDefendRed(thisPtr);
         return copyObj;
     },
 };
 
 export const BasicDefendRed = (thisPtr: NativePointer): NativePointer => {
-    let wrapCard = AbstractCard.NewCardCtor("BasicDefend_R", "鲜血护盾", "red/skill/defend", 1, "失去2点生命，获得 !B! 点格挡。", CardType.SKILL,
-        CardColor.RED, CardRarity.BASIC, CardTarget.SELF, DamageType.NORMAL, vfuncs);
-    wrapCard.baseBlock = 9;
+    const wrapCard = AbstractCard.NewCardCtor("BasicDefend_R", "鲜血护盾", "red/skill/defend", 1, "获得 !B! 点格挡。 NL 被消耗时获得 !M! 层金属化。", CardType.SKILL, CardColor.RED, CardRarity.BASIC, CardTarget.SELF, DamageType.NORMAL, vfuncs);
+    wrapCard.baseBlock = 5;
+    wrapCard.baseMagicNumber = 1;
+    wrapCard.magicNumber = 1;
 
     return wrapCard.rawPtr;
 };
