ba0933b02e8e2f173a36bf05789ee1668354a9d8
CCTV-1 <script.tar.gz@gmail.com>
2023-05-24 19:09:45 +0800

    change to each turn give a card.

--- ModScripts/NewRelics/TheArcaneRing.ts
+++ ModScripts/NewRelics/TheArcaneRing.ts
@@ -5,22 +5,20 @@ import { NativeActions } from "../NativeFuncWrap/NativeActions.js";
 import { CardType, LandingSound, RelicTier } from "../enums.js";
 
 const vfuncs: NewRelicVFuncType = {
-    atBattleStart: (thisPtr: NativePointer) => {
+    atTurnStart: (thisPtr: NativePointer) => {
         const wrapRelic = new AbstractRelic(thisPtr);
-        for (let index = 0; index < 3; index++) {
-            const randCardPtr = NativeAbstractDungeon.returnTrulyRandomCardInCombat(CardType.SKILL);
-            const wrapCard = new AbstractCard(randCardPtr);
-            const copyCard = new AbstractCard(wrapCard.makeCopy());
-            if (copyCard.cost >= 0) {
-                copyCard.costForTurn = 0;
-                if (copyCard.costForTurn != copyCard.cost) {
-                    copyCard.isCostModifiedForTurn = true;
-                }
-                copyCard.exhaust = true;
+        const randCardPtr = NativeAbstractDungeon.returnTrulyRandomCardInCombat(CardType.SKILL);
+        const wrapCard = new AbstractCard(randCardPtr);
+        const copyCard = new AbstractCard(wrapCard.makeCopy());
+        if (copyCard.cost >= 0) {
+            copyCard.costForTurn = 0;
+            if (copyCard.costForTurn != copyCard.cost) {
+                copyCard.isCostModifiedForTurn = true;
             }
-            const addCardToHandAction = NativeActions.common.MakeTempCardInHand.Ctor(copyCard.rawPtr, 1, false);
-            wrapRelic.addToBot(addCardToHandAction);
+            copyCard.exhaust = true;
         }
+        const addCardToHandAction = NativeActions.common.MakeTempCardInHand.Ctor(copyCard.rawPtr, 1, false);
+        wrapRelic.addToBot(addCardToHandAction);
         wrapRelic.flash();
     },
     makeCopy: (thisPtr: NativePointer) => {
@@ -30,7 +28,7 @@ const vfuncs: NewRelicVFuncType = {
 };
 
 export const TheArcaneRing = (thisPtr: NativePointer): NativePointer => {
-    let relicObj = AbstractRelic.NewRelicCtor("TheArcaneRing", "奥术魔戒", "在战斗开始时添加三张耗能为0的技能牌到你手上。 NL 它们额外具有 #y消耗。", "魔网魔力泄漏的源头之一。", "TheArcaneRing.png", RelicTier.BOSS, LandingSound.HEAVY, vfuncs);
+    let relicObj = AbstractRelic.NewRelicCtor("TheArcaneRing", "奥术魔戒", "每回合添加一张耗能为0的技能牌到你手上。 NL 它们额外具有 #y消耗。", "魔网魔力泄漏的源头之一。", "TheArcaneRing.png", RelicTier.BOSS, LandingSound.HEAVY, vfuncs);
 
     return relicObj;
 };
