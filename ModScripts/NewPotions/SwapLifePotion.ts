599d43f9c7eaa3403681f738396c149fcf8496d1
CCTV-1 <script.tar.gz@gmail.com>
2023-06-27 18:40:56 +0800

    now can effect elite/boss enemy,but the ability degradation to: absorb 15 life.

--- ModScripts/NewPotions/SwapLifePotion.ts
+++ ModScripts/NewPotions/SwapLifePotion.ts
@@ -5,6 +5,7 @@ import { AbstractRoom } from "../NativeClassWrap/AbstractRoom.js";
 import { NativeAbstractDungeon } from "../NativeFuncWrap/NativeAbstractDungeon.js";
 import { NativeHelpers } from "../NativeFuncWrap/NativeHelpers.js";
 import { NativeSTDLib } from "../NativeFuncWrap/NativeSTDLib.js";
+import { NativeVFX } from "../NativeFuncWrap/NativeVFX.js";
 import { EnemyType, PotionColor, PotionRarity, PotionSize, RoomPhase } from "../enums.js";
 
 const vfunc: NewPotionVFuncType = {
@@ -12,22 +13,37 @@ const vfunc: NewPotionVFuncType = {
         const currentRoom = new AbstractRoom(NativeAbstractDungeon.getCurrRoom());
         if (currentRoom.phase === RoomPhase.COMBAT) {
             const wrapMonster = new AbstractMonster(targetCreature);
-            if (wrapMonster.type === EnemyType.NORMAL) {
-                const currentPlayer = AbstractDungeon.getInstance().player;
-                const playerDiff = wrapMonster.currentHealth - currentPlayer.currentHealth;
-                const monsterDiff = currentPlayer.currentHealth - wrapMonster.currentHealth;
-                currentPlayer.heal2(playerDiff);
-                wrapMonster.heal2(monsterDiff);
+            const currentPlayer = AbstractDungeon.getInstance().player;
+            switch (wrapMonster.type) {
+                case EnemyType.NORMAL: {
+                    const playerDiff = wrapMonster.currentHealth - currentPlayer.currentHealth;
+                    const monsterDiff = currentPlayer.currentHealth - wrapMonster.currentHealth;
+                    currentPlayer.heal2(playerDiff);
+                    wrapMonster.heal2(monsterDiff);
+                    return;
+                }
+                case EnemyType.ELITE:
+                case EnemyType.BOSS: {
+                    const effectList = AbstractDungeon.getInstance().effectList;
+                    const thoughtBubbleEffect = NativeVFX.ThoughtBubble.Ctor(currentPlayer.dialogX, currentPlayer.dialogY, 3.0, "这个敌人过于强大，药剂只汲取了它的15点生命。", true);
+                    NativeSTDLib.ArrayList.AbstractGameEffect.Add(effectList, thoughtBubbleEffect);
+                    currentPlayer.heal2(15);
+                    wrapMonster.heal2(-15);
+                    return ;
+                }
+                default: {
+                    //???
+                }
             }
         }
     },
     initializeData: (thisPtr: NativePointer) => {
         const wrapPotion = new AbstractPotion(thisPtr);
         wrapPotion.potency = 0;
-        wrapPotion.description = "与一个普通怪物交换当前血量。"
+        wrapPotion.description = "与一个怪物交换当前血量。"
         let potionTips = wrapPotion.tips;
         NativeSTDLib.ArrayList.PowerTip.clear(potionTips);
-        let newTip = NativeHelpers.PowerTip.Ctor("换血药剂", "与一个普通怪物交换当前血量。");
+        let newTip = NativeHelpers.PowerTip.Ctor("换血药剂", "与一个怪物交换当前血量。");
         NativeSTDLib.ArrayList.PowerTip.add(potionTips, newTip);
     },
     getPotency: (thisPtr: NativePointer, ascensionLevel: number) => {
